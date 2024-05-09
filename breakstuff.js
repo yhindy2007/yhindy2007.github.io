f9a075c061f8a9b7fd87056a3f7838eb13926934
yhindy2007 <yhindy2007@gmail.com>
2024-05-09 13:04:42 -0400

    Create breakstuff.js

---
+++ breakstuff.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const inputElement = document.querySelector('.composer__input__d6OQi');
+    if (inputElement) {
+        inputElement.setAttribute('data-placeholder', 'look I changed');
+    }
+});
